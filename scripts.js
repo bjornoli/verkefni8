84d5d35f7918d0b3ba637c1488ecb1de0a72833b
bjornoli <boe15@hi.is>
2019-11-08 21:00:46 +0000

    uppfæra

--- scripts.js
+++ scripts.js
@@ -14,25 +14,58 @@ const text = (() => {
     items = _items;
     _form.addEventListener('submit', formHandler);
 
-    // TODO láta hluti í _items virka
+    for(const item of items.querySelectorAll('.item')){
+      const checkbox = item.querySelector('input');
+      checkbox.addEventListener('change', finish);
+      const span = item.querySelector('span');
+      span.addEventListener('click', edit);
+      const del = item.querySelector('button');
+      del.addEventListener('click', deleteItem);
+    }
   }
 
   function formHandler(e) {
     e.preventDefault();
+
+    var list = document.getElementsByClassName('items');
+    var input = document.getElementsByClassName('form__input');
+    var li = document.createElement("li");
+    var check = document.createElement("input");
+    var span = document.createElement("span");
+    var button = document.createElement("button");
+    check.setAttribute("class", "item__checkbox");
+    span.setAttribute("class", "item__text");
+    li.setAttribute('class', "item");
+    button.setAttribute("class", "item__button");
+    li.appendChild(document.createTextNode(e));
 
     console.log('halló heimur');
   }
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+    const checkbox = e.target;
+    const parent = checkbox.parentNode;
+
+    parent.classList.toggle('item--done');
   }
 
   // event handler fyrir það að breyta færslu
-  function edit(e) {
+  function edit(e) {    
+    const first = e.target;
+    console.log(first);
+    const text = first.innerHTML;
+    console.log(text);
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'searchField');
+    input.setAttribute('required', true);
+    first.el = input;
   }
 
   // event handler fyrir það að klára að breyta færslu
   function commit(e) {
+    
   }
 
   // fall sem sér um að bæta við nýju item
@@ -41,6 +74,10 @@ const text = (() => {
 
   // event handler til að eyða færslu
   function deleteItem(e) {
+    const button = e.target;
+    const parent = button.parentNode;
+
+    parent.parentNode.removeChild(parent);
   }
 
   // hjálparfall til að útbúa element
